1afa23bce440ce0dcda1dfb806d182645a8408bd
Ciro Mattia Gonano <ciromattia@gmail.com>
2013-01-29 04:04:06 +0100

    Remove obsolete droplet

--- syncDroplet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf KindleComicConverter.app/Contents/Resources/*
-cp -a resources/Scripts resources/description.rtfd resources/droplet.rsrc KindleComicConverter.app/Contents/Resources/
-cp resources/Info.plist KindleComicConverter.app/Contents/
-cp resources/comic2ebook.icns KindleComicConverter.app/Contents/Resources/droplet.icns
-cp kcc/*.py KindleComicConverter.app/Contents/Resources/
-cp `which unrar` KindleComicConverter.app/Contents/Resources/
