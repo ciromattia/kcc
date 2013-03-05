244d6bbb8b020bdfe8c65297f6ca842ecbb17a78
Ciro Mattia Gonano <ciromattia@gmail.com>
2013-03-05 11:27:50 +0100

    Code cleanup

--- kcc/pdfjpgextract.py
+++ kcc/pdfjpgextract.py
@@ -18,11 +18,12 @@
 # PERFORMANCE OF THIS SOFTWARE.
 #
 
-__license__   = 'ISC'
+__license__ = 'ISC'
 __copyright__ = '2012-2013, Ciro Mattia Gonano <ciromattia@gmail.com>'
 __docformat__ = 'restructuredtext en'
 
 import os
+
 
 class PdfJpgExtract:
     def __init__(self, origFileName):
@@ -48,14 +49,14 @@ class PdfJpgExtract:
             istream = pdf.find("stream", i)
             if istream < 0:
                 break
-            istart = pdf.find(startmark, istream, istream+20)
+            istart = pdf.find(startmark, istream, istream + 20)
             if istart < 0:
-                i = istream+20
+                i = istream + 20
                 continue
             iend = pdf.find("endstream", istart)
             if iend < 0:
                 raise Exception("Didn't find end of stream!")
-            iend = pdf.find(endmark, iend-20)
+            iend = pdf.find(endmark, iend - 20)
             if iend < 0:
                 raise Exception("Didn't find end of JPG!")
 
@@ -63,10 +64,9 @@ class PdfJpgExtract:
             iend += endfix
             print "JPG %d from %d to %d" % (njpg, istart, iend)
             jpg = pdf[istart:iend]
-            jpgfile = file(self.path+"/jpg%d.jpg" % njpg, "wb")
+            jpgfile = file(self.path + "/jpg%d.jpg" % njpg, "wb")
             jpgfile.write(jpg)
             jpgfile.close()
 
             njpg += 1
             i = iend
-
