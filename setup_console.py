3365f111e415ed062cb09f551ce12a85152f510d
Paweł Jastrzębski <pawelj@vulturis.eu>
2013-03-07 09:26:41 +0100

    setup_console.py py2exe -> cx_freeze

--- setup_console.py
+++ setup_console.py
@@ -1,14 +1,22 @@
 """
-py2app/py2exe build script for KCC No-GUI release.
+cx_freeze build script for Windows KCC No-GUI release.
 
 Usage (Windows):
-    python setup.py py2exe
+    python setup_console.py build
 """
-from distutils.core import setup
 import sys
-import py2exe
+from cx_Freeze import setup, Executable
 sys.path.insert(0, 'kcc')
 
-setup(
-        console=['kcc/comic2ebook.py', 'kcc/kindlestrip.py'],
-)
+setup(  name = "KindleComicConverter",
+        version = "2.6",
+        author = "Ciro Mattia Gonano",
+        author_email = "ciromattia@gmail.com",
+        description = "A tool to convert comics (CBR/CBZ/PDFs/image folders) to MOBI.",
+        license= " ISC License (ISCL)",
+        keywords= "kindle comic mobipocket mobi cbz cbr manga",
+        url = "http://github.com/ciromattia/kcc",
+        options = {"build_exe": {}},
+        executables = [Executable("kcc/comic2ebook.py", appendScriptToExe=True, appendScriptToLibrary=False),
+		               Executable("kcc/kindlestrip.py", appendScriptToExe=True, appendScriptToLibrary=False)]
+        )
