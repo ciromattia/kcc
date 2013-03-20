d346ca04666301511cf2a42a80634acc46653fe9
Ciro Mattia Gonano <ciromattia@gmail.com>
2013-03-20 10:39:42 +0100

    Update version and readme

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ use_setuptools()
 import sys
 
 NAME = "KindleComicConverter"
-VERSION = "2.8-dev"
+VERSION = "2.8"
 MAIN = "kcc.py"
 
 includefiles = ['README.md', 'MANIFEST.in', 'LICENSE.txt', 'comic2ebook.ico', 'comic2ebook.icns']
