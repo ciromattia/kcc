1dada3cf8dfdcddb555f4fa76eefa9b8a1579541
Paweł Jastrzębski <pawelj@vulturis.eu>
2013-09-29 11:05:02 +0200

    OSX: After packaging PATH is still broken

--- kcc.py
+++ kcc.py
@@ -34,6 +34,7 @@ except ImportError:
 from kcc import KCC_gui
 from multiprocessing import freeze_support
 if sys.platform.startswith('darwin'):
+    os.environ['PATH'] = '/usr/local/bin:' + os.environ['PATH']
     from kcc import KCC_ui_osx as KCC_ui
 elif sys.platform.startswith('linux'):
     from kcc import KCC_ui_linux as KCC_ui
