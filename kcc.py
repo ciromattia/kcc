e57225812194c0973930873670e5c3763fec18a7
Paweł Jastrzębski <pawelj@vulturis.eu>
2013-12-06 23:33:04 +0100

    Version Bump

--- kcc.py
+++ kcc.py
@@ -18,7 +18,7 @@
 # TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
 
-__version__ = '3.6'
+__version__ = '3.6.1'
 __license__ = 'ISC'
 __copyright__ = '2012-2013, Ciro Mattia Gonano <ciromattia@gmail.com>, Pawel Jastrzebski <pawelj@vulturis.eu>'
 __docformat__ = 'restructuredtext en'
