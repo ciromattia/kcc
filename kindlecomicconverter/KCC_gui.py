77afa77d32f42275ba8962e51fa0623ec35a54cc
Alex Xu <alexkurosakimh3@gmail.com>
2023-08-07 10:53:38 -0700

    Display kindlegen path (#571)
    * add %LOCALAPPDATA%\\Amazon\\KC2
    
    * fix kindlegen link
    
    * expandvars
    
    * remove link
    
    * print where kindlegen
    
    * adjust where command
    
    * remove platform

--- kindlecomicconverter/KCC_gui.py
+++ kindlecomicconverter/KCC_gui.py
@@ -19,6 +19,7 @@
 import json
 import os
 import re
+import subprocess
 import sys
 from urllib.parse import unquote
 from urllib.request import urlretrieve, urlopen
@@ -859,6 +860,12 @@ class KCCGUI(KCC_ui.Ui_mainWindow):
                         self.addMessage('Your <a href="https://www.amazon.com/b?node=23496309011">KindleGen</a>'
                                         ' is outdated! MOBI conversion might fail.', 'warning')
                     break
+            where_command = 'where kindlegen.exe'
+            if os.name == 'posix':
+                where_command = 'which kindlegen'
+            process = subprocess.run(where_command, stdout=PIPE, stderr=STDOUT, stdin=PIPE, shell=True)
+            locations = process.stdout.decode('utf-8').split('\n')
+            self.addMessage(f"<b>KindleGen Found:</b> {locations[0]}", 'info')
         else:
             self.kindleGen = False
             if startup:
