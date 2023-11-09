431862a2e9ba2353829e6c9ee32189ed2ffda74e
Alex Xu <alexkurosakimh3@gmail.com>
2023-11-09 14:27:09 -0800

    fix files with ' or " by using arg lists instead of strings (#581)
    * replace popen with subprocess run
    
    * add splitlines
    
    * remove stdin
    
    * fix xml
    
    * fix error logging and 7zip

--- kindlecomicconverter/comicarchive.py
+++ kindlecomicconverter/comicarchive.py
@@ -22,7 +22,6 @@ import os
 import platform
 import subprocess
 import distro
-from psutil import Popen
 from shutil import move
 from subprocess import STDOUT, PIPE
 from xml.dom.minidom import parseString
@@ -35,19 +34,17 @@ class ComicArchive:
         self.type = None
         if not os.path.isfile(self.filepath):
             raise OSError('File not found.')
-        process = Popen('7z l -y -p1 "' + self.filepath + '"', stderr=STDOUT, stdout=PIPE, stdin=PIPE, shell=True)
-        for line in process.stdout:
-            if b'Type =' in line:
-                self.type = line.rstrip().decode().split(' = ')[1].upper()
+        process = subprocess.run(['7z', 'l', '-y', '-p1', self.filepath], stderr=STDOUT, stdout=PIPE, encoding='UTF-8')
+        for line in process.stdout.splitlines():
+            if 'Type =' in line:
+                self.type = line.rstrip().split(' = ')[1].upper()
                 break
-        process.communicate()
         if process.returncode != 0 and distro.id() == 'fedora':
-            process = Popen('unrar l -y -p1 "' + self.filepath + '"', stderr=STDOUT, stdout=PIPE, stdin=PIPE, shell=True)
-            for line in process.stdout:
-                if b'Details: ' in line:
-                    self.type = line.rstrip().decode().split(' ')[1].upper()
+            process = subprocess.run(['unrar', 'l', '-y', '-p1', self.filepath], stderr=STDOUT, stdout=PIPE, encoding='UTF-8')
+            for line in process.stdout.splitlines():
+                if 'Details: ' in line:
+                    self.type = line.rstrip().split(' ')[1].upper()
                     break
-            process.communicate()
             if process.returncode != 0:
                 raise OSError('Archive is corrupted or encrypted.')
             elif self.type not in ['7Z', 'RAR', 'RAR5', 'ZIP']:
@@ -58,22 +55,20 @@ class ComicArchive:
     def extract(self, targetdir):
         if not os.path.isdir(targetdir):
             raise OSError('Target directory doesn\'t exist.')
-        process = Popen('7z x -y -xr!__MACOSX -xr!.DS_Store -xr!thumbs.db -xr!Thumbs.db -o"' + targetdir + '" "' +
-                        self.filepath + '"', stdout=PIPE, stderr=STDOUT, stdin=PIPE, shell=True)
-        process.communicate()
+        process = subprocess.run(['7z', 'x', '-y', '-xr!__MACOSX', '-xr!.DS_Store', '-xr!thumbs.db', '-xr!Thumbs.db', '-o' + targetdir, self.filepath],
+                                 stdout=PIPE, stderr=STDOUT, encoding='UTF-8')
         if process.returncode != 0 and distro.id() == 'fedora':
-            process = Popen('unrar x -y -x__MACOSX -x.DS_Store -xthumbs.db -xThumbs.db "' + self.filepath + '" "' +
-                    targetdir + '"', stdout=PIPE, stderr=STDOUT, stdin=PIPE, shell=True)
-            process.communicate()
+            process = subprocess.run(['unrar', 'x', '-y', '-x__MACOSX', '-x.DS_Store', '-xthumbs.db', '-xThumbs.db', self.filepath, targetdir] 
+                    , stdout=PIPE, stderr=STDOUT)
             if process.returncode != 0:
                 raise OSError('Failed to extract archive.')
         elif process.returncode != 0 and platform.system() == 'Darwin':
-            process = subprocess.run(f"unar '{self.filepath}' -f -o '{targetdir}'", 
-                stdout=PIPE, stderr=STDOUT, stdin=PIPE, shell=True)
+            process = subprocess.run(['unar', self.filepath, '-f', '-o', targetdir], 
+                stdout=PIPE, stderr=STDOUT, encoding='UTF-8')
             if process.returncode != 0:
-                raise Exception(process.stdout.decode("utf-8"))
+                raise Exception(process.stdout)
         elif process.returncode != 0:
-            raise OSError('Failed to extract archive. Check if p7zip-rar is installed.')
+            raise OSError(process.stdout.strip())
         tdir = os.listdir(targetdir)
         if 'ComicInfo.xml' in tdir:
             tdir.remove('ComicInfo.xml')
@@ -86,19 +81,17 @@ class ComicArchive:
     def addFile(self, sourcefile):
         if self.type in ['RAR', 'RAR5']:
             raise NotImplementedError
-        process = Popen('7z a -y "' + self.filepath + '" "' + sourcefile + '"',
-                        stdout=PIPE, stderr=STDOUT, stdin=PIPE, shell=True)
-        process.communicate()
+        process = subprocess.run(['7z', 'a', '-y', self.filepath, sourcefile],
+                        stdout=PIPE, stderr=STDOUT)
         if process.returncode != 0:
             raise OSError('Failed to add the file.')
 
     def extractMetadata(self):
-        process = Popen('7z x -y -so "' + self.filepath + '" ComicInfo.xml',
-                        stdout=PIPE, stderr=STDOUT, stdin=PIPE, shell=True)
-        xml = process.communicate()
+        process = subprocess.run(['7z', 'x', '-y', '-so', self.filepath, 'ComicInfo.xml'],
+                        stdout=PIPE, stderr=STDOUT, encoding='UTF-8')
         if process.returncode != 0:
             raise OSError('Failed to extract archive.')
         try:
-            return parseString(xml[0])
+            return parseString(process.stdout)
         except ExpatError:
             return None
