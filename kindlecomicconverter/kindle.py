d7cd269b8c28a3b8b49580fdb8c6f20e3f490695
Alex Xu <alexkurosakimh3@gmail.com>
2024-08-04 21:33:09 -0700

    add APFS to external drive

--- kindlecomicconverter/kindle.py
+++ kindlecomicconverter/kindle.py
@@ -31,7 +31,7 @@ class Kindle:
     def findDevice(self):
         for drive in reversed(psutil.disk_partitions(False)):
             if (drive[2] == 'FAT32' and drive[3] == 'rw,removable') or \
-               (drive[2] in ('vfat', 'msdos', 'FAT') and 'rw' in drive[3]):
+               (drive[2] in ('vfat', 'msdos', 'FAT', 'apfs') and 'rw' in drive[3]):
                 if os.path.isdir(os.path.join(drive[1], 'system')) and \
                         os.path.isdir(os.path.join(drive[1], 'documents')):
                     return drive[1]
