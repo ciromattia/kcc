a4460d0f2567f6eaa8364cb78a1f978bebd47e9e
Alex Xu <alexkurosakimh3@gmail.com>
2025-04-02 17:24:42 -0700

    math

--- kindlecomicconverter/image.py
+++ kindlecomicconverter/image.py
@@ -392,8 +392,9 @@ class ComicPage:
     def maybeCrop(self, box, minimum):
         w, h = self.image.size
         left, upper, right, lower = box
-        ratio = 1 - self.opt.preservemargin / 100
-        box = left * ratio, upper * ratio, right + (w - right) * (1 - ratio), lower + (h - lower) * (1 - ratio)
+        if self.opt.preservemargin:
+            ratio = 1 - self.opt.preservemargin / 100
+            box = left * ratio, upper * ratio, right + (w - right) * (1 - ratio), lower + (h - lower) * (1 - ratio)
         box_area = (box[2] - box[0]) * (box[3] - box[1])
         image_area = self.image.size[0] * self.image.size[1]
         if (box_area / image_area) >= minimum:
