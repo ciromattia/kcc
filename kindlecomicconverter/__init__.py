3517994d3736fcf75b09008084bb01732e5caac9
Alex Xu <alexkurosakimh3@gmail.com>
2025-04-17 08:19:06 -0700

    bump 7.4.0

--- kindlecomicconverter/__init__.py
+++ kindlecomicconverter/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '7.3.3'
+__version__ = '7.4.0'
 __license__ = 'ISC'
 __copyright__ = '2012-2022, Ciro Mattia Gonano <ciromattia@gmail.com>, Pawel Jastrzebski <pawelj@iosphe.re>, darodi'
 __docformat__ = 'restructuredtext en'
