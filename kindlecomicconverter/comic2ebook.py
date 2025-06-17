952504a6ef32de160b4318cb2c9127feccd5deb6
9783e6 <143286922+9783e6@users.noreply.github.com>
2025-06-18 02:35:28 +0300

    Optimization for JPEG non-color pages in forcecolor mode

--- kindlecomicconverter/comic2ebook.py
+++ kindlecomicconverter/comic2ebook.py
@@ -642,8 +642,8 @@ def imgFileProcessing(work):
             img.autocontrastImage()
             img.resizeImage()
             img.optimizeForDisplay(opt.reducerainbow)
-            if (opt.forcepng and not opt.forcecolor) or (opt.forcepng and opt.forcecolor and not workImg.color):
-                img.quantizeImage()
+            if not opt.forcecolor or (opt.forcecolor and not workImg.color):
+                img.convertToGrayscaleOrQuantize()
             output.append(img.saveToDir())
         return output
     except Exception:
