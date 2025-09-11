8a862f11ac778ef59a1336c2af0f9384c7679990
Alex Xu <alexkurosakimh3@gmail.com>
2025-09-11 14:20:09 -0700

    bump to 9.1.0

--- kindlecomicconverter/__init__.py
+++ kindlecomicconverter/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '9.0.0'
+__version__ = '9.1.0'
 __license__ = 'ISC'
 __copyright__ = '2012-2022, Ciro Mattia Gonano <ciromattia@gmail.com>, Pawel Jastrzebski <pawelj@iosphe.re>, darodi'
 __docformat__ = 'restructuredtext en'
