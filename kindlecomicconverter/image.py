06194b33addc548a73f3dc824c93bd9236c7fd4b
Alex Xu <alexkurosakimh3@gmail.com>
2025-10-20 11:42:59 -0700

    disable default gamma correction/darkening of 1.8 (1.0 is disabled) (#1030)
    * disable default gamma correction of 1.8 to 1.0 (disabled)
    
    * update gamma tooltip
    
    * custom gamma

--- kindlecomicconverter/image.py
+++ kindlecomicconverter/image.py
@@ -85,22 +85,22 @@ class ProfileData:
     ]
 
     ProfilesKindleEBOK = {
-        'K1': ("Kindle 1", (600, 670), Palette4, 1.8),
-        'K2': ("Kindle 2", (600, 670), Palette15, 1.8),
-        'KDX': ("Kindle DX/DXG", (824, 1000), Palette16, 1.8),
-        'K34': ("Kindle Keyboard/Touch", (600, 800), Palette16, 1.8),
-        'K57': ("Kindle 5/7", (600, 800), Palette16, 1.8),
-        'KPW': ("Kindle Paperwhite 1/2", (758, 1024), Palette16, 1.8),
-        'KV': ("Kindle Voyage", (1072, 1448), Palette16, 1.8),
+        'K1': ("Kindle 1", (600, 670), Palette4, 1.0),
+        'K2': ("Kindle 2", (600, 670), Palette15, 1.0),
+        'KDX': ("Kindle DX/DXG", (824, 1000), Palette16, 1.0),
+        'K34': ("Kindle Keyboard/Touch", (600, 800), Palette16, 1.0),
+        'K57': ("Kindle 5/7", (600, 800), Palette16, 1.0),
+        'KPW': ("Kindle Paperwhite 1/2", (758, 1024), Palette16, 1.0),
+        'KV': ("Kindle Voyage", (1072, 1448), Palette16, 1.0),
     }
 
     ProfilesKindlePDOC = {
-        'KPW34': ("Kindle Paperwhite 3/4/Oasis", (1072, 1448), Palette16, 1.8),
-        'K810': ("Kindle 8/10", (600, 800), Palette16, 1.8),
-        'KO': ("Kindle Oasis 2/3/Paperwhite 12/Colorsoft 12", (1264, 1680), Palette16, 1.8),
-        'K11': ("Kindle 11", (1072, 1448), Palette16, 1.8),
-        'KPW5': ("Kindle Paperwhite 5/Signature Edition", (1236, 1648), Palette16, 1.8),
-        'KS': ("Kindle Scribe", (1860, 2480), Palette16, 1.8),
+        'KPW34': ("Kindle Paperwhite 3/4/Oasis", (1072, 1448), Palette16, 1.0),
+        'K810': ("Kindle 8/10", (600, 800), Palette16, 1.0),
+        'KO': ("Kindle Oasis 2/3/Paperwhite 12/Colorsoft 12", (1264, 1680), Palette16, 1.0),
+        'K11': ("Kindle 11", (1072, 1448), Palette16, 1.0),
+        'KPW5': ("Kindle Paperwhite 5/Signature Edition", (1236, 1648), Palette16, 1.0),
+        'KS': ("Kindle Scribe", (1860, 2480), Palette16, 1.0),
     }
 
     ProfilesKindle = {
@@ -109,35 +109,35 @@ class ProfileData:
     }
 
     ProfilesKobo = {
-        'KoMT': ("Kobo Mini/Touch", (600, 800), Palette16, 1.8),
-        'KoG': ("Kobo Glo", (768, 1024), Palette16, 1.8),
-        'KoGHD': ("Kobo Glo HD", (1072, 1448), Palette16, 1.8),
-        'KoA': ("Kobo Aura", (758, 1024), Palette16, 1.8),
-        'KoAHD': ("Kobo Aura HD", (1080, 1440), Palette16, 1.8),
-        'KoAH2O': ("Kobo Aura H2O", (1080, 1430), Palette16, 1.8),
-        'KoAO': ("Kobo Aura ONE", (1404, 1872), Palette16, 1.8),
-        'KoN': ("Kobo Nia", (758, 1024), Palette16, 1.8),
-        'KoC': ("Kobo Clara HD/Kobo Clara 2E", (1072, 1448), Palette16, 1.8),
-        'KoCC': ("Kobo Clara Colour", (1072, 1448), Palette16, 1.8),
-        'KoL': ("Kobo Libra H2O/Kobo Libra 2", (1264, 1680), Palette16, 1.8),
-        'KoLC': ("Kobo Libra Colour", (1264, 1680), Palette16, 1.8),
-        'KoF': ("Kobo Forma", (1440, 1920), Palette16, 1.8),
-        'KoS': ("Kobo Sage", (1440, 1920), Palette16, 1.8),
-        'KoE': ("Kobo Elipsa", (1404, 1872), Palette16, 1.8),
+        'KoMT': ("Kobo Mini/Touch", (600, 800), Palette16, 1.0),
+        'KoG': ("Kobo Glo", (768, 1024), Palette16, 1.0),
+        'KoGHD': ("Kobo Glo HD", (1072, 1448), Palette16, 1.0),
+        'KoA': ("Kobo Aura", (758, 1024), Palette16, 1.0),
+        'KoAHD': ("Kobo Aura HD", (1080, 1440), Palette16, 1.0),
+        'KoAH2O': ("Kobo Aura H2O", (1080, 1430), Palette16, 1.0),
+        'KoAO': ("Kobo Aura ONE", (1404, 1872), Palette16, 1.0),
+        'KoN': ("Kobo Nia", (758, 1024), Palette16, 1.0),
+        'KoC': ("Kobo Clara HD/Kobo Clara 2E", (1072, 1448), Palette16, 1.0),
+        'KoCC': ("Kobo Clara Colour", (1072, 1448), Palette16, 1.0),
+        'KoL': ("Kobo Libra H2O/Kobo Libra 2", (1264, 1680), Palette16, 1.0),
+        'KoLC': ("Kobo Libra Colour", (1264, 1680), Palette16, 1.0),
+        'KoF': ("Kobo Forma", (1440, 1920), Palette16, 1.0),
+        'KoS': ("Kobo Sage", (1440, 1920), Palette16, 1.0),
+        'KoE': ("Kobo Elipsa", (1404, 1872), Palette16, 1.0),
     }
 
     ProfilesRemarkable = {
-        'Rmk1': ("reMarkable 1", (1404, 1872), Palette16, 1.8),
-        'Rmk2': ("reMarkable 2", (1404, 1872), Palette16, 1.8),
-        'RmkPP': ("reMarkable Paper Pro", (1620, 2160), Palette16, 1.8),
-        'RmkPPMove': ("reMarkable Paper Pro Move", (954, 1696), Palette16, 1.8),
+        'Rmk1': ("reMarkable 1", (1404, 1872), Palette16, 1.0),
+        'Rmk2': ("reMarkable 2", (1404, 1872), Palette16, 1.0),
+        'RmkPP': ("reMarkable Paper Pro", (1620, 2160), Palette16, 1.0),
+        'RmkPPMove': ("reMarkable Paper Pro Move", (954, 1696), Palette16, 1.0),
     }
 
     Profiles = {
         **ProfilesKindle,
         **ProfilesKobo,
         **ProfilesRemarkable,
-        'OTHER': ("Other", (0, 0), Palette16, 1.8),
+        'OTHER': ("Other", (0, 0), Palette16, 1.0),
     }
 
 
