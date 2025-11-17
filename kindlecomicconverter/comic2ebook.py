74f244e7ac42dcf3b64c587fce761b4664ff5079
Alex Xu <alexkurosakimh3@gmail.com>
2025-11-17 12:04:10 -0800

    Revert "fix -o in c2e (#1161)"
    This reverts commit 9429bed91c184a85f6e4c0b4f7c01d785bba51fb.

--- kindlecomicconverter/comic2ebook.py
+++ kindlecomicconverter/comic2ebook.py
@@ -922,8 +922,11 @@ def getOutputFilename(srcpath, wantedname, ext, tomenumber):
     if wantedname is not None:
         if wantedname.endswith(ext):
             filename = os.path.abspath(wantedname)
+        elif os.path.isdir(srcpath):
+            filename = os.path.join(os.path.abspath(options.output), os.path.basename(srcpath) + ext)
         else:
-            filename = os.path.abspath(wantedname) + ext
+            filename = os.path.join(os.path.abspath(options.output),
+                                    os.path.basename(os.path.splitext(srcpath)[0]) + ext)
     elif os.path.isdir(srcpath):
         filename = srcpath + tomenumber + ext
     else:
