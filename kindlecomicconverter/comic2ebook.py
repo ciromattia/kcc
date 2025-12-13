f91aa920a6fd6efbf592ad05ebff45d725a7ada1
jaroslawjanas <jaroslawjanas.dev@gmail.com>
2025-12-13 02:33:52 +0100

    Fix for conflicting short flags

--- kindlecomicconverter/comic2ebook.py
+++ kindlecomicconverter/comic2ebook.py
@@ -1317,7 +1317,7 @@ def makeParser():
     main_options.add_argument("--ts", "--targetsize", type=int, dest="targetsize", default=None,
                               help="the maximal size of output file in MB."
                                    " [Default=100MB for webtoon and 400MB for others]")
-    main_options.add_argument("-q", "--jpeg-quality", type=int, dest="jpegquality", default=85,
+    main_options.add_argument("--jpeg-quality", type=int, dest="jpegquality", default=85,
                               help="JPEG compression quality (1-100). Higher values produce better quality "
                                    "but larger files. [Default=85]")
 
