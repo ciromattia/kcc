0f630c5804cf369c0e1a9eb0128caca4c4bc9e3d
Alex Xu <alexkurosakimh3@gmail.com>
2026-09-06 20:56:32 -0700

    fix 7zip warning

--- kindlecomicconverter/comicarchive.py
+++ kindlecomicconverter/comicarchive.py
@@ -99,7 +99,7 @@ class ComicArchive:
                 pass
 
         if missing:
-            raise OSError(f'Extraction failed, install <a href="https://github.com/ciromattia/kcc#7-zip">specialized extraction software.</a>  ')
+            raise UserWarning(f'Extraction failed, install <a href="https://github.com/ciromattia/kcc#7-zip">specialized extraction software.</a>  ')
         else:
             raise OSError(EXTRACTION_ERROR)
 
